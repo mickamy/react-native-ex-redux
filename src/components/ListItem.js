4ea35cbdaf939334dd81d020bf73a475ca6bc9b4
Tetsuro Mikami <tetsuro_mikami@wamazing.jp>
2017-07-02 06:38:52 +0900

    Present library title list

---
+++ src/components/ListItem.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+import { CardSection } from './common';
+
+class ListItem extends Component {
+  render() {
+    const { title, description } = this.props.library
+    const { titleStyle } = styles
+    return (
+      <CardSection>
+        <Text style={titleStyle}>
+          {title}
+        </Text>
+      </CardSection>
+    );
+  }
+}
+
+const styles = {
+  titleStyle: {
+    fontSize: 18,
+    paddingLeft: 15,
+    paddingRight: 15,
+  },
+};
+
+export default ListItem;
